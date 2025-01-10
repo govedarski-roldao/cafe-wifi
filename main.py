d5a402bee7657eeded29147d6df8af6238a264bf
Rodolfo Govedarski-Roldao <r_roldao@hotmail.com>
2025-01-10 02:00:05 +0200

    add route inside of main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
@@ -13,6 +13,11 @@ class Base(DeclarativeBase):
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
