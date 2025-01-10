5bc49bb34a28ff6f4696ffa584f9117edc5cd57c
Rodolfo Govedarski-Roldao <r_roldao@hotmail.com>
2025-01-10 22:04:49 +0200

    add presentation of the coffee places and creation of table

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from enum import unique
-from tokenize import String
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, Boolean, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from flask_ckeditor import CKEditor
+from flask import jsonify
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap5(app)
 
 
 class Base(DeclarativeBase):
@@ -16,7 +20,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-#base table for the coffee's list
+
+# base table for the coffee's list
 class Cafe(db.Model):
     __tablename__ = "cafe"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -25,17 +30,26 @@ class Cafe(db.Model):
     img_url: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     location: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    has_toilets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
     has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
     seats: Mapped[str] = mapped_column(String(250), nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+print(app.config['SQLALCHEMY_DATABASE_URI'])
 
 
 @app.route("/")
 def home():
     result = db.session.execute(db.select(Cafe))
     cafes = result.scalars().all()
-    return render_template("index.html")
+    for cafe in cafes:
+        print(cafe.name, cafe.location, cafe.coffee_price)
+
+    return render_template("index.html", coffee_list=cafes)
 
 
 if __name__ == '__main__':
