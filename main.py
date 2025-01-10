6c4719344b52f9eb3ada93e5ff403921d5400ddd
Rodolfo Govedarski-Roldao <r_roldao@hotmail.com>
2025-01-10 12:54:41 +0200

    create tablebase

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from enum import unique
+from tokenize import String
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, Boolean, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
 
@@ -9,13 +12,29 @@ class Base(DeclarativeBase):
     pass
 
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+#base table for the coffee's list
+class Cafe(db.Model):
+    __tablename__ = "cafe"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    map_url: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    location: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_toilets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    seats: Mapped[str] = mapped_column(String(250), nullable=False)
+    coffee_price: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
 @app.route("/")
 def home():
+    result = db.session.execute(db.select(Cafe))
+    cafes = result.scalars().all()
     return render_template("index.html")
 
 
